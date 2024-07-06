f7d2cdce38077221474a5d50dc107ac4bd64e1c0
MariamIssah <m.issah1@alustudent.com>
2024-07-06 02:40:10 -0700

    2-recurse.py

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,33 +1,24 @@
-#!/usr/bin/python
-"""
-Reddit API module to recursively get all hot post titles of a subreddit.
-"""
+#!/usr/bin/python3
+""" 2-recurse.py """
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """
-    Returns a list of titles of all hot posts for a subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-        hot_list (list): A list to store the titles of hot posts.
-        after (str): The after parameter for pagination.
-
-    Returns:
-        list: A list of titles of all hot posts, or None if invalid.
-    """
+    """ returns list with titles of all hot articles in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'MyBot/1.0'}
     params = {"limit": 100, "after": after}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
+    response = requests.get(
+                            url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
     if response.status_code == 200:
-        data = response.json().get("data", {})
-        hot_list += [post.get("data", {}).get("title", "None") for post in data.get("children", [])]
-        after = data.get("after")
-        if after is None:
+        data = response.json()["data"]
+        hot_list += [post["data"]["title"] for post in data["children"]]
+        if data["after"] is None:
             return hot_list
-        return recurse(subreddit, hot_list, after)
+        else:
+            return recurse(subreddit, hot_list, data["after"])
     elif response.status_code == 404:
         return None
